dacc749b2fe4cfa1f28a42d9bfcfd2e47d2add28
rifqi <67531472+Reeaveld@users.noreply.github.com>
2023-11-23 09:34:05 +0700

    Update index.js, keluoaan seharusnya udh bisa muncul gambar

--- UI_demonslayer (NEWEST)/index.js
+++ UI_demonslayer (NEWEST)/index.js
@@ -39,7 +39,7 @@ const highlightMenu = () => {
     }
   };
 
-  document.getElementById('home-page').addEventListener('click', function() {
+  document.getElementById('navbar__logo').addEventListener('click', function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
   
@@ -56,4 +56,34 @@ const highlightMenu = () => {
   };
   
   menuLinks.addEventListener('click', hideMobileMenu);
-  navLogo.addEventListener('click', hideMobileMenu);  
+  navLogo.addEventListener('click', hideMobileMenu);
+
+  // Carousel/Slideshow
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("alur__img--container");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
